26f89e880ce9ac5d240549ecd242270005cfc115
YusufTarek <yusuftarek18turk@gmail.com>
2025-08-04 19:29:07 +0300

    Add MCP_OBJECT class to handle Durable Object state and requests

---
+++ src/MCP_OBJECT.ts
@@ -0,0 +1,13 @@
+export class MCP_OBJECT {
+  state: DurableObjectState;
+  env: any;
+
+  constructor(state: DurableObjectState, env: any) {
+    this.state = state;
+    this.env = env;
+  }
+
+  async fetch(request: Request): Promise<Response> {
+    return new Response("MCP_OBJECT is working!");
+  }
+}
